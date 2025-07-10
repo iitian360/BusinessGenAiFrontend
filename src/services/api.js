4019189efc2ba82fcfc22474919e2691f6852fd2
rahulsoni10 <rahulrudra12345@gmail.com>
2025-07-10 23:40:09 +0530

    API route change

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 
 import axios from 'axios';
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
-
+import { API_BASE_URL } from '../constants/apiRoutes';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
